4c028ff21b0243629d997c428ea4e470062c8b7c
snumse <dlagurwns03@gmail.com>
2025-03-01 11:23:37 +0900

    Add files via upload

--- sevennet_tutorial/MD.py
+++ sevennet_tutorial/MD.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../../SevenNet')
 import os
 working_dir = os.getcwd() # save current path
+working_dir = os.path.join(working_dir, 'experiment')
 
 """Demonstrates molecular dynamics with constant energy."""
 
@@ -30,7 +31,7 @@ atoms = traj[0]
 # Describe the interatomic interactions with the Effective Medium Theory
 if mode == 'df':
     from sevenn_df.sevennet_calculator import SevenNetCalculator
-    atoms.calc = SevenNetCalculator(os.path.join(working_dir, 'checkpoint_best_full5.pth'))
+    atoms.calc = SevenNetCalculator(os.path.join(working_dir, 'checkpoint_best_base_full5.pth'))
 elif mode == '7net-0':
     from sevenn.sevennet_calculator import SevenNetCalculator
     atoms.calc = SevenNetCalculator(model='7net-0', device='cpu')
@@ -40,8 +41,27 @@ MaxwellBoltzmannDistribution(atoms, temperature_K=300)
 
 # We want to run MD with constant energy using the VelocityVerlet algorithm.
 dyn = VelocityVerlet(atoms, 2 * units.fs)  # 2 fs time step.
+if mode == 'df':
+    dyn.run(1000)
+else:
+    dyn.run(50)
 
+#########################################################
 
+atoms = traj[0]
+if mode == 'df':
+    from sevenn_df.sevennet_calculator import SevenNetCalculator
+    atoms.calc = SevenNetCalculator(os.path.join(working_dir, 'checkpoint_best_base_full5.pth'))
+elif mode == '7net-0':
+    from sevenn.sevennet_calculator import SevenNetCalculator
+    atoms.calc = SevenNetCalculator(model='7net-0', device='cpu')
+
+# Set the momenta corresponding to T=300K
+MaxwellBoltzmannDistribution(atoms, temperature_K=300)
+
+# We want to run MD with constant energy using the VelocityVerlet algorithm.
+dyn = VelocityVerlet(atoms, 2 * units.fs)  # 2 fs time step.
+    
 Etot = []
 def printenergy(a=atoms):  # store a reference to atoms in the definition.
     """Function to print the potential, kinetic and total energy."""
@@ -52,6 +72,7 @@ def printenergy(a=atoms):  # store a reference to atoms in the definition.
         #   'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin), flush=True)
 
 
+dyn = VelocityVerlet(atoms, 2 * units.fs)  # 2 fs time step.
 # Now run the dynamics
 dyn.attach(printenergy, interval=1)
 # We also want to save the positions of all atoms after every 100th time step.
@@ -77,7 +98,7 @@ def plot_data(Etot):
     plt.plot(Etot, label='Etot')
 
     # Adding labels and title
-    plt.xlabel('Time (2ps per tick)')
+    plt.xlabel('Time (2fs per tick)')
     plt.ylabel('Energy per atom (eV)')
     plt.title('Fluctuation of Etot with Time')
     plt.legend()
--- sevennet_tutorial/experiment/md/MD_check.py
+++
@@ -1,8 +0,0 @@
-from ase.io import read
-from ase.visualize import view
-from ase.io import write
-
-# Read the trajectory file
-traj = read('moldyn3.traj', index=':')
-
-write('movie.gif', traj, interval=10)
